bcbbf8c728a3710eaa6db9b222c3a4e8f0e10000
harishammache <hammache_haris@hotmail.fr>
2024-01-25 11:14:25 +0000

    add condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    exit()
-number_element = len(sys.argv) - 1
+    number_element = len(sys.argv) - 1
 
-if number_element == 0:
-    print("{} arguments.".format(number_element))
+    if number_element == 0:
+        print("{} arguments.".format(number_element))
 
-elif number_element == 1:
-    print("{} argument:".format(number_element))
+    elif number_element == 1:
+        print("{} argument:".format(number_element))
 
-elif number_element > 1:
-    print("{} arguments:".format(number_element))
-index = 0
+    elif number_element > 1:
+        print("{} arguments:".format(number_element))
+    index = 0
 
-for arg in sys.argv:
-    if index != 0:
-        print("{}: {}".format(index, arg))
-    index += 1
+    for arg in sys.argv:
+        if index != 0:
+            print("{}: {}".format(index, arg))
+        index += 1
