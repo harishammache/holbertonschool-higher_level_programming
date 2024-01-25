829b299033285340f370ed0d4d3c8bffce4f9e9d
harishammache <hammache_haris@hotmail.fr>
2024-01-25 11:10:48 +0000

    Write a program that prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+number_element = len(sys.argv) - 1
+
+if number_element == 0:
+	print("{} arguments.".format(number_element))
+
+elif number_element == 1:
+	print("{} argument:".format(number_element))
+
+elif number_element > 1:
+	print("{} arguments:".format(number_element))
+
+index = 0
+
+for arg in sys.argv:
+	if index != 0:
+		print ("{}: {}".format(index, arg))
+	index += 1
