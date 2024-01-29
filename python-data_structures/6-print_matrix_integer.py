206dab3ae1c2c877843e1ad9038c05511961230f
harishammache <hammache_haris@hotmail.fr>
2024-01-29 13:26:09 +0000

    add :d at the format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for line_matrice in matrix:
         for number in line_matrice:
             if number == line_matrice[-1]:
-                print("{}".format(number), end="")
+                print("{:d}".format(number), end="")
             else:
-                print("{} ".format(number), end="")
+                print("{:d} ".format(number), end="")
         print()
