6e99daaeb6983752f135f4327719fe330a0a5517
harishammache <hammache_haris@hotmail.fr>
2024-01-29 11:50:22 +0000

    Write a function that prints all integers of a list.

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import re
 def no_c(my_string):
-    new_string = re.sub('[c,C]','',my_string)
+    new_string = ''
+    for index in my_string:
+        if index != 'C' and index != 'c':
+            new_string += index
     return new_string
