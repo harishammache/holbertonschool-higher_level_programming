21e2376cc82a6268523d5f6e056e7f9152097c12
harishammache <hammache_haris@hotmail.fr>
2024-01-30 15:21:55 +0000

    change condition

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
+    for key in sorted(a_dictionary.keys()):
+        value = a_dictionary[key]
         print("{} : {}".format(key, value))
