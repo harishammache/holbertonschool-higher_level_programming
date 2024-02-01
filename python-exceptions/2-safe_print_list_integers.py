2897669568ecc81fc0ce4b26f1d2cd016b4a3a96
harishammache <hammache_haris@hotmail.fr>
2024-02-01 11:59:12 +0000

    change the syntax of the code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for index in my_list[:x]:
+
+    for index in my_list[:x]:
+        try:
             if type(index) is not int:
                 continue
             else:
                 print("{:d}".format(index), end="")
                 count += 1
-    except (IndexError, TypeError, ValueError):
-        print()
-    finally:
-        print()
+        except (IndexError, TypeError, ValueError):
+            continue
+    print()
     return count
