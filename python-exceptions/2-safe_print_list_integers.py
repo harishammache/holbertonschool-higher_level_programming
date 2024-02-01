2e8049a2b72e450ee2da0b6e6bde8733bbb8e882
harishammache <hammache_haris@hotmail.fr>
2024-02-01 11:54:10 +0000

    regroup the except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,16 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for index in my_list[:x]:
-            if type(index) is not int:
-                continue
-            else:
+            if type(index) is int:
                 print("{:d}".format(index), end="")
                 count += 1
-    except IndexError:
-        pass
-    except TypeError:
-        pass
-    except ValueError:
+    except (IndexError, TypeError, ValueError):
         pass
     finally:
         print()
