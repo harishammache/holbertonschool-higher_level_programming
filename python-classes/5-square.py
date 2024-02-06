a175b0a858dc7c095f3a0ca23ec9f64a90ea036e
harishammache <hammache_haris@hotmail.fr>
2024-02-06 08:54:29 +0000

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a Square class."""
+
+
+class Square:
+    """Class Square with a size attribute."""
+    def __init__(self, size=0):
+        self.size = size
+    """Getter for retrieving the value of the private attribute 'size'"""
+    @property
+    def size(self):
+        return self.__size
+    """Setter method for setting the value of the private attribute 'size'"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """public class Square."""
+    def area(self):
+        return self.__size ** 2
+    """Prints the square using '#' characters."""
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
