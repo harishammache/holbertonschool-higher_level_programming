674cbbbfaa7df1c22be7b3dbf9e9ae30de1041f1
harishammache <hammache_haris@hotmail.fr>
2024-02-07 14:42:10 +0000

    delete 1 condition

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,8 +10,6 @@ def print_square(size):
     """
         prints a square with the character #.
     """
-    if type(size) is not int and size < 0:
-        raise ValueError("size must be >= 0")
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
