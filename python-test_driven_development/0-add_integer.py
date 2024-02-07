ac35753271cc0c249158f0ef70dfcadf76023086
harishammache <hammache_haris@hotmail.fr>
2024-02-07 09:49:36 +0000

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""sum of 2 integer"""
+"""
+    verify if a is an integer or a float
+    converts float to int
+    result: sum of 2 integer
+"""
 
 
 def add_integer(a, b=98):
-    """verify if a is an integer or a float"""
+    """
+        verify if a is an integer or a float
+    """
     if not type(a) in [int, float]:
         raise TypeError("a must be an integer or float")
     if not type(b) in [int, float]:
