c73079b4d19beae2cada973de96477e7857288e0
harishammache <hammache_haris@hotmail.fr>
2024-02-07 15:03:15 +0000

    change message erreur

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
         verify if a is an integer or a float
     """
     if not type(a) in [int, float]:
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
     if not type(b) in [int, float]:
-        raise TypeError("b must be an integer or float")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
