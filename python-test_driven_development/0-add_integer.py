703d4201c2d7d07acd73458c987096dd3553d6ee
harishammache <hammache_haris@hotmail.fr>
2024-02-07 07:55:28 +0000

    pycodestyle problème

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer or float")
     if not type(b) in [int, float]:
         raise TypeError("b must be an integer or float")
-    if type(a) is  float:
+    if type(a) is float:
         a = int(a)
-    if type(b) is  float:
+    if type(b) is float:
         b = int(b)
     return a + b
