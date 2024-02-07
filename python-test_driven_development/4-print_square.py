061737cfab75ad32bd504d0a3e041f72c87168fa
harishammache <hammache_haris@hotmail.fr>
2024-02-07 14:10:53 +0000

    Write a function that prints a square with the character #.

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    prints a square with the character #.
+    one argument: size
+    size is the size length of the sqaure
+"""
+
+
+def print_square(size):
+    """
+        prints a square with the character #.
+    """
+    if type(size) is not int and size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for index in range(size):
+        print("#" * size)
