4fd7cfd3abe87eb34d5f39be601ebaf3357cd590
harishammache <hammache_haris@hotmail.fr>
2024-02-08 11:05:40 +0000

    add commentary

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -15,26 +15,31 @@ class TestMaxInteger(unittest.TestCase):
         test for know who is the max
     """
     def all_Integer(self):
+        """Test with a list of integers"""
         list = [1, 2, 3, 4, 5]
         result = max_integer(list)
         self.assertEqual(result, 5)
 
     def negativ_Integer(self):
+        """Test with a list containing negative integers"""
         list = [-1, -3, -9]
         result = max_integer(list)
         self.assertEqual(result, -1)
 
     def empty_list(self):
+        """Test with an empty list"""
         list = []
         result = max_integer(list)
         self.assertEqual(result, None)
 
     def float_integer(self):
+        """Test with float"""
         list = [1.2, 5.2, 8.9]
         result = max_integer(list)
         self.assertEqual(result, 8.9)
 
     def list_str(self):
+        """Test with caractere and integer"""
         list = ["a", "b", 1]
         self.assertRaises(TypeError, max_integer)
 
