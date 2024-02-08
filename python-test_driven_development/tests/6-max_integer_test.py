7cd3a6b594df980f39640d2c3519f367d7b1695e
harishammache <hammache_haris@hotmail.fr>
2024-02-08 11:38:50 +0000

    new test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,37 +5,29 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """
-        test for know who is the max
-    """
-    def test_all_Integer(self):
-        """Test with a list of integers"""
-        list = [1, 2, 3, 4, 5]
-        result = max_integer(list)
-        self.assertEqual(result, 5)
+    def test_positive_numbers(self):
+        """Test with a list of positive integers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_negativ_Integer(self):
-        """Test with a list containing negative integers"""
-        list = [-1, -3, -9]
-        result = max_integer(list)
-        self.assertEqual(result, -1)
+    def test_negative_numbers(self):
+        """Test with a list of negative integers"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_numbers(self):
+        """Test with a list of mixed positive and negative integers"""
+        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
 
     def test_empty_list(self):
         """Test with an empty list"""
-        list = []
-        result = max_integer(list)
-        self.assertEqual(result, None)
+        self.assertIsNone(max_integer([]))
 
-    def test_float_integer(self):
-        """Test with float"""
-        list = [1.2, 5.2, 8.9]
-        result = max_integer(list)
-        self.assertEqual(result, 8.9)
+    def test_float_numbers(self):
+        """Test with a list of float numbers"""
+        self.assertEqual(max_integer([1.1, 2.2, 3.3, 4.4]), 4.4)
 
-    def test_list_str(self):
-        """Test with caractere and integer"""
-        list = ["a", "b", 1]
-        self.assertRaises(TypeError, max_integer)
+    def test_mixed_list(self):
+        """Test with a list of mixed data types"""
+        self.assertRaises(TypeError, max_integer, ["a", "b", 1])
 
 if __name__ == '__main__':
     unittest.main()
