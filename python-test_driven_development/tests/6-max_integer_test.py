22d8be5bb0333b229ba8464fd8c01cc666f49f0f
harishammache <hammache_haris@hotmail.fr>
2024-02-08 11:09:08 +0000

    change name of function

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,32 +11,34 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-   
-    def all_Integer(self):
+    """
+        test for know who is the max
+    """
+    def test_all_Integer(self):
         """Test with a list of integers"""
         list = [1, 2, 3, 4, 5]
         result = max_integer(list)
         self.assertEqual(result, 5)
 
-    def negativ_Integer(self):
+    def test_negativ_Integer(self):
         """Test with a list containing negative integers"""
         list = [-1, -3, -9]
         result = max_integer(list)
         self.assertEqual(result, -1)
 
-    def empty_list(self):
+    def test_empty_list(self):
         """Test with an empty list"""
         list = []
         result = max_integer(list)
         self.assertEqual(result, None)
 
-    def float_integer(self):
+    def testfloat_integer(self):
         """Test with float"""
         list = [1.2, 5.2, 8.9]
         result = max_integer(list)
         self.assertEqual(result, 8.9)
 
-    def list_str(self):
+    def test_list_str(self):
         """Test with caractere and integer"""
         list = ["a", "b", 1]
         self.assertRaises(TypeError, max_integer)
