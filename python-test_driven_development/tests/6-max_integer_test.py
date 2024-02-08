757176a9d6ec0a9759e2ac36ffcaac2ea2b8aaff
harishammache <hammache_haris@hotmail.fr>
2024-02-08 10:23:09 +0000

    pycodestyle

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,10 @@
 """
     Since the beginning you have been creating “Interactive tests”
     def max_integer(list=[])
+    class TestMaxInteger
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
