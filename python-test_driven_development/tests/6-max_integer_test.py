3dbc0b7ecaab27ef5d301c5ca9d0be152f594f70
harishammache <hammache_haris@hotmail.fr>
2024-02-08 11:32:23 +0000

    change the file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-    Since the beginning you have been creating “Interactive tests”
-    def max_integer(list=[])
-    class TestMaxInteger
-"""
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-
 
 class TestMaxInteger(unittest.TestCase):
     """
