67cd95611a048016ec7883c82e676e3e64e17aa0
harishammache <hammache_haris@hotmail.fr>
2024-02-09 10:55:49 +0000

    Write a class Rectangle that defines a rectangle

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""area and perimeter rectangle"""
+
+
+class Rectangle:
+    """define a class"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        perimeter = (self.__width + self.__height) * 2
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return perimeter
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rectangle_str = ""
+            for _ in range(self.__height):
+                rectangle_str += "#" * self.__width + "\n"
+            return rectangle_str.rstrip("\n")
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
