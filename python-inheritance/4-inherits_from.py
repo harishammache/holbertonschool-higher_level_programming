29e411ec4c3d7d722adaa2f81c3dcd59d1f6d562
harishammache <hammache_haris@hotmail.fr>
2024-02-13 11:41:53 +0000

    pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Write a function that returns
-    True if the object is an instance 
+    True if the object is an instance
     of a class that inherited (directly or indirectly)
     from the specified class ; otherwise False.
 """
