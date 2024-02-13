881403bb7a3f535eb4b3a464061fdf97b441421e
harishammache <hammache_haris@hotmail.fr>
2024-02-13 14:18:55 +0000

    Write a class Rectangle that inherits from BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    create an empty class
+"""
+
+
+class BaseGeometry:
+    """
+        Public instance method
+    """
+
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+        class Rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.integer_validator("width", self.__width)
+        self.__height = height
+        self.integer_validator("height", self.__height)
