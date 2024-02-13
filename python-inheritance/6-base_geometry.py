a7cd765a8d11ea79ad7505b4a72fd64fa0c2205c
harishammache <hammache_haris@hotmail.fr>
2024-02-13 12:04:38 +0000

    Write a class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    create an empty class
+"""
+
+
+class BaseGeometry:
+    """
+        Public instance method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
