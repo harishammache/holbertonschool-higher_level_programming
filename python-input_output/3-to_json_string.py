465c8a5a83574bda4dc0fdf2ff0dc8fe8b3d4f7d
harishammache <hammache_haris@hotmail.fr>
2024-02-14 13:20:18 +0000

    delete commentary

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import json
-"""
-     returns the JSON representation of an object (string)
-"""
 
 
 def to_json_string(my_obj):
-    """returns the JSON representation"""
     return json.dumps(my_obj)
