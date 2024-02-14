3a9ae14c46d5165042ebcb186814eed7e85a4d4f
harishammache <hammache_haris@hotmail.fr>
2024-02-14 07:16:21 +0000

    delete the class mum and import the file 7-base_geometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
     create an empty class
 """
-
-
-class BaseGeometry:
-    """
-        Public instance method
-    """
-
-    def area(self):
-        """method for calculated area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method for validate if a num is integer"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
