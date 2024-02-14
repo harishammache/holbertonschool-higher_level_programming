71774efaa768459e0c1d5eb636cefe45aad8da4e
harishammache <hammache_haris@hotmail.fr>
2024-02-14 07:05:11 +0000

    change def init

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -25,8 +25,9 @@ class Rectangle(BaseGeometry):
     """
         class Rectangle that inherits from BaseGeometry
     """
+
     def __init__(self, width, height):
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("width", self.__width)
+        self.integer_validator("height", height)
         self.__height = height
-        self.integer_validator("height", self.__height)
