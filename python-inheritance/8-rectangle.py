07b3159e12864059fd33608b9d73d8af83edfe58
harishammache <hammache_haris@hotmail.fr>
2024-02-14 07:23:52 +0000

    new mum function

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    create an empty class
+"""
+
+
+class BaseGeometry:
+    """
+        Public instance method
+    """
+
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
-    """ Class that defines a rectangle from BaseGeometry Class """
+    """
+        class Rectangle that inherits from BaseGeometry.
+    """
 
     def __init__(self, width, height):
-        """ Initializes instance """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """Instantiation with width and height"""
         self.__width = width
+        self.integer_validator("width", self.__width)
         self.__height = height
+        self.integer_validator("height", self.__height)
