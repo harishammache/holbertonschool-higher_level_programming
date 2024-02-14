194c29e3b8487370e7a4b67f97e2ecd44817c4fa
harishammache <hammache_haris@hotmail.fr>
2024-02-14 12:45:15 +0000

    change code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Instantiation with width and height"""
-        super().integer_validator("width", self.__width)
-        super().integer_validator("height", self.__height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
