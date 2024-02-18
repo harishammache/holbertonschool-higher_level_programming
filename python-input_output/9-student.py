0214ee32b6fa64905cb84c050b33b65230c846c3
harishammache <hammache_haris@hotmail.fr>
2024-02-18 12:00:20 +0000

    add the class student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,16 +4,18 @@
 """
 
 
-def __init__(self, first_name, last_name, age):
-    """
-        Public instance attributes:
-            first_name
-            last_name
-            age
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+class Student:
+    """class to define student"""
+    def __init__(self, first_name, last_name, age):
+        """
+            Public instance attributes:
+                first_name
+                last_name
+                age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """ retrieves a dictionary representation of a Student instance"""
