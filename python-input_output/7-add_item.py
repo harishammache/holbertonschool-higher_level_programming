13bcb9a1ca1a2373046330c7dc2105bd91588371
harishammache <hammache_haris@hotmail.fr>
2024-02-18 10:44:09 +0000

    change if and else to try and exept

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,9 +11,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 
-if load_from_json_file(filename):
+try:
     my_list = load_from_json_file(filename)
-else:
+except FileNotFoundError:
     my_list = []
 python_list = sys.argv[1:]
 save_to_json_file(my_list, filename)
