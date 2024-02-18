0f8232f9ceb38eb58e5c1dac192a2f5b7f3112f0
harishammache <hammache_haris@hotmail.fr>
2024-02-18 10:44:49 +0000

    pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ import sys
     , and then save them to a file
 """
 
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
