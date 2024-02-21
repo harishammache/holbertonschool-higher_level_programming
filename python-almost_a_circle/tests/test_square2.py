f24a9e11dbb2890a3a3eeb117eb690d4d4638f5b
harishammache <hammache_haris@hotmail.fr>
2024-02-21 13:28:46 +0000

    test for the task 11

---
+++ python-almost_a_circle/tests/test_square2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Unittest for file square.py
+"""
+import unittest
+from models.base import Base
+from models.square import Square
+
+
+class TestId(unittest.TestCase):
+    """all test"""
+    def setUp(self):
+        """Reset Base._Base__nb_objects before each test"""
+        Base._nb_objects = 0
+    
+    def Square_size(self):
+        """test with just a size same argument"""
+        s1 = Square(5)
+        self.assertEqual(str(s1), "[Square] (1) 0/0 - 5")
+        self.assertEqual((s1.size), 5)
+
+    def Square_no_int(self):
+        """test with the size is not an int"""
+        with self.assertRaises(TypeError):
+            s1 = Square("haris")
+
+
+if __name__ == '__main__':
+    unittest.main()
