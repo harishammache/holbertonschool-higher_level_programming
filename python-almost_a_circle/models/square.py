638a3d3ad4299a0d52a0ebfdd688faf8c52f91ff
harishammache <hammache_haris@hotmail.fr>
2024-02-22 08:31:39 +0000

    add function dictionary

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,3 +36,6 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
