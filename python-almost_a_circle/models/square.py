f2d59584e1451ef94e5292656ccc2f4fa0fb95d6
harishammache <hammache_haris@hotmail.fr>
2024-02-22 08:33:42 +0000

    add commentary

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,4 +38,5 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ public method"""
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
