4bf8f1510dcbbc269afcecccc5fe24c8be2e98f6
harishammache <hammache_haris@hotmail.fr>
2024-02-22 09:21:48 +0000

    add ""

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,6 +19,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """static method"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
