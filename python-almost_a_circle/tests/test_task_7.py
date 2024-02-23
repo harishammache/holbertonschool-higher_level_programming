b07489ca9d4cce1590673bcd73673c689484a6d7
harishammache <hammache_haris@hotmail.fr>
2024-02-23 14:33:17 +0000

    test task 7

---
+++ python-almost_a_circle/tests/test_task_7.py
@@ -0,0 +1,24 @@
+import unittest
+import io
+import sys
+from models.rectangle import Rectangle
+from unittest.mock import patch
+
+
+class TestRectangleDisplay(unittest.TestCase):
+    def test_rectangle_display(self):
+        r1 = Rectangle(2, 3, 2, 2)
+        expected_output = "  ##\n  ##\n  ##\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            r1.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+
+        r2 = Rectangle(3, 2, 1, 0)
+        expected_output = " ###\n ###\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            r2.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
