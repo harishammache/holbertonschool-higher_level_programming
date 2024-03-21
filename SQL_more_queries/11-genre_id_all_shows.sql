fc309918d08c955f7c6481abab632212e33268f8
harishammache <hammache_haris@hotmail.fr>
2024-03-21 15:34:25 +0000

    add ASC

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv_show.title, tv_show_genres.genre_id 
 FROM tv_shows 
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-ORDER BY tv_show.title, tv_show_genres.genre_id;
+ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
