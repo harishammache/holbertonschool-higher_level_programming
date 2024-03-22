4c991d2093036e31c7719d0599fc7babe9bd028b
harishammache <hammache_haris@hotmail.fr>
2024-03-22 11:55:25 +0000

    lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ from sys import argv
 import MySQLdb
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passw=argv[2], data=argv[3])
+                         passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     stats = cursor.fetchall()
