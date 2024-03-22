64ec463c70c897671b172c57390dd531abfbbc06
harishammache <hammache_haris@hotmail.fr>
2024-03-22 12:49:08 +0000

    spaces

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[0],
                                  passwd=argv[1], db=argv[2])
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM  states WHERE name LIKE 'N%'\
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'\
                    ORDER BY id ASC")
     stats = cursor.fetchall()
     for stat in stats:
