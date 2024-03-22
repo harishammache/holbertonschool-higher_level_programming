bf1f9207c4ceadddd16b13bc5f0ea851646622f2
harishammache <hammache_haris@hotmail.fr>
2024-03-22 12:07:12 +0000

    change pycodstyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
-
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
 from sys import argv
 import MySQLdb
-
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                                  passwd=argv[2], db=argv[3])
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    stats = cursor.fetchall()
+    curs = connection.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    stats = curs.fetchall()
     for stat in stats:
         print(stat)
-    cursor.close()
+    curs.close()
     connection.close()
