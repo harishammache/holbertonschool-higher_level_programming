368b8d61bcc88072047bcaa178fca62721eb7664
harishammache <hammache_haris@hotmail.fr>
2024-03-22 12:18:20 +0000

    file task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,10 @@
 """
 lists all states from the database hbtn_0e_0_usa
 """
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
