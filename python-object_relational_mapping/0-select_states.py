a4b85e86ad7303359c2293b2220909419473921c
harishammache <hammache_haris@hotmail.fr>
2024-03-22 11:33:15 +0000

    change syntax

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,11 @@ import sys
 """ lists all states from the database hbtn_0e_0_usa"""
 
 
-def list_states(username, password, database):
+if __name__ == "__main__":
     """function list_states with 3 arguments"""
     connection = MySQLdb.connect(host='localhost', port='3306',
-                                 user=username, passw=password, data=database)
+                                 user=sys.argv[1], passw=sys.argv[2],
+                                 data=sys.argv[3])
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     stats = cursor.fetchall()
@@ -15,11 +16,3 @@ def list_states(username, password, database):
         print(stat)
     cursor.close()
     connection.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    data = sys.argv[3]
-
-    list_states(username, password, data)
