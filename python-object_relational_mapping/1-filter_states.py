56cc8e9703355596d3ebf14f71c851879a8317ff
harishammache <hammache_haris@hotmail.fr>
2024-03-22 12:52:45 +0000

    commentary syntax

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N from the database"""
+""" 
+lists all states with a name starting with N from the database
+"""
 from sys import argv
 import MySQLdb
 
