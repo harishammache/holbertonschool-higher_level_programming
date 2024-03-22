8b6f3bd36162b8b3df9fc6418b2d4e40b2a8e427
harishammache <hammache_haris@hotmail.fr>
2024-03-22 12:50:18 +0000

    change argv

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,8 +4,8 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[0],
-                                 passwd=argv[1], db=argv[2])
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                 passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'\
                    ORDER BY id ASC")
