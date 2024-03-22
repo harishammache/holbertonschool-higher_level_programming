4b8485c57ea557d26324f9350149e81b7fc1a65f
harishammache <hammache_haris@hotmail.fr>
2024-03-22 11:36:07 +0000

    syntax change

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,6 @@ lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    """function list_states with 3 arguments"""
     connection = MySQLdb.connect(host='localhost', port='3306',
                                  user=sys.argv[1], passw=sys.argv[2],
                                  data=sys.argv[3])
