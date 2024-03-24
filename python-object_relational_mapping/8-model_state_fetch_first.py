e61c70eadb36926a24c948c374591638ede0b7d2
harishammache <hammache_haris@hotmail.fr>
2024-03-24 10:23:35 +0000

    prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all State objects from the database hbtn_0e_6_usa"""
+"""prints the first State object from the database hbtn_0e_6_usa"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,12 +9,11 @@ if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
     data = argv[3]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, data),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id)
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    states = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(states.id, states.name))
