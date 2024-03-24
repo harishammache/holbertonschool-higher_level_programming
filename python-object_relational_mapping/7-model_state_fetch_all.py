9210afd3fd1c0c743e8192bd48c69473897ba5e9
harishammache <hammache_haris@hotmail.fr>
2024-03-24 09:55:53 +0000

    change the argument

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ lists all State objects from the database hbtn_0e_6_usa"""
 from sys import argv
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
     username = argv[0]
     password = argv[1]
     data = argv[2]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(username, password, data,
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
