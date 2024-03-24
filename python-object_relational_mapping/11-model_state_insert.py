4f27ef69affd6fe5dc4986bc8b9d8201fb51ae50
harishammache <hammache_haris@hotmail.fr>
2024-03-24 11:21:43 +0000

    delete port

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
     data = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(username, password, data),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
