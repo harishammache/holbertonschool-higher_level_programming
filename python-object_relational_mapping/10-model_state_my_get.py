bdd4869c72dfc94ced2d579a1f0f2306abb6a3b5
harishammache <hammache_haris@hotmail.fr>
2024-03-24 11:02:40 +0000

    prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    data = argv[3]
+    state_name = argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.name == state_name).first()
+
+    if states is not None:
+        print(states.id)
+    else:
+        print("Not found")
